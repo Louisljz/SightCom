33484d4381bd6d88e9e5af833d61ee85ece2df30
Louis JZ <louis.ljz08@gmail.com>
2023-05-27 19:00:07 +0700

    scene desc

---
+++ SeeingAI/scene_desc.py
@@ -0,0 +1,53 @@
+from ultralytics import YOLO
+from ultralytics.yolo.utils.plotting import Annotator
+import numpy as np
+import openai
+import os
+
+class DescribeScene:
+    def __init__(self):
+        self.model = YOLO('yolov8n.pt')
+        openai.api_key = os.environ['Seeing AI GPT Key']
+       
+    def detect_objects(self, img):
+        img = np.array(img)
+        results = self.model.predict(img, verbose=False)
+
+        labels = []
+        for r in results:
+            annotator = Annotator(img)
+            boxes = r.boxes
+            for box in boxes:
+                b = box.xyxy[0]
+                cls = self.model.names[int(box.cls)]
+                labels.append(cls)
+                annotator.box_label(b, cls, color=(255, 0, 0)) 
+
+        anot = annotator.result()
+        return anot, labels
+
+    def describe(self, labels):
+        prompt = f'''
+        create a scene description (one short sentence) from these labels 
+        detected by YOLO model: {labels}
+        Don't include uncertain info.
+        '''
+        completion = openai.ChatCompletion.create(
+                        model = 'gpt-3.5-turbo',
+                        messages = [
+                            {'role': 'user', 'content': prompt}
+                        ],
+                        temperature = 0)
+
+        return completion['choices'][0]['message']['content']
+
+if __name__ == "__main__":
+    import pyttsx3
+    from PIL import Image
+    scene = DescribeScene()
+    engine = pyttsx3.init()
+    img = Image.open('test.jpg')
+    anot, labels = scene.detect_objects(img)
+    desc = scene.describe(labels)
+    engine.say(desc)
+    engine.runAndWait()
---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from PIL import Image
+from SeeingAI.scene_desc import DescribeScene
+
+st.set_page_config('Seeing AI Prototype', ':eyes:')
+st.title('Seeing AI Prototype :eyes:')
+
+tab1, tab2 = st.tabs(['Scene Description', 'Currency Recognition'])
+
+with tab1:
+    scene = DescribeScene()
+    buffer = st.camera_input('Take a picture of your surrounding!')
+    if buffer:
+        img = Image.open(buffer)
+        anot, labels = scene.detect_objects(img)
+        desc = scene.describe(labels)
+        st.image(anot)
+        st.text(desc)
